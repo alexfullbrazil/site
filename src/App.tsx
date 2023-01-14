b2adcf27aad452a22e45bd2b8279a9d32d7f4857
AlexFull <alexfull2@gmail.com>
2023-01-14 10:21:28 -0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "/src/styles/app.scss";
+import request from "graphql-request";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  
+
+  return <div className="container">bla</div>;
+}
+
+export default App;
