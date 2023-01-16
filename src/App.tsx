8af5d6b1e166a17ed342e10d0f9cb6d75ed5cae1
AlexFull <alexfull2@gmail.com>
2023-01-16 09:54:03 -0300

    all ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "/src/styles/app.scss";
-import request from "graphql-request";
+// import '/src/styles/app.scss';
+import { useGetPostsQuery } from './generated/graphql';
 
 function App() {
-  const [data, setData] = useState([]);
+  const {
+    data: dataPosts,
+    loading,
+    error,
+  } = useGetPostsQuery({
+    variables: {},
+  });
 
-  
-
-  return <div className="container">bla</div>;
+  return (
+    <>
+      <ul>
+        {dataPosts?.posts?.map((item) => {
+          return (
+            <li key={item.id}>
+              <h2>{item.title}</h2>
+              <p>{item.excerpt}</p>
+              <img src={item.coverImage?.url} alt={item.title} />
+              <div dangerouslySetInnerHTML={{ __html: item.content.html }} />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import '/src/styles/index.scss'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import '/src/styles/index.scss';
+
+import { ApolloProvider } from '@apollo/client';
+import { client } from './contexts/graphql-context';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
