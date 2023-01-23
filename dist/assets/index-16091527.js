ccce589ab7ab1fe41963521ab8e0f13e0933920e
AlexFull <alexfull2@gmail.com>
2023-01-23 10:28:25 -0300

    all ok

---
+++ dist/assets/index-16091527.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-491c37fb.js";function r(){return e("div",{children:"Home"})}export{r as default};
---
+++ dist/assets/index-d8b27217.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-491c37fb.js";function n(){return o("div",{children:"Not Found"})}export{n as default};
